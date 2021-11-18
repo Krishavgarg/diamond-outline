5553939e8e4b0c6f0c44d2fdf1f985d51b71e185
AVIRAL108 <vrlgarg@gmail.com>
2021-11-18 22:36:31 +0530

    first commit

---
+++ p11.java
@@ -0,0 +1,25 @@
+class p11
+{public static void main()
+    {String a=" ";int s=1;int h=0;
+    {for(int i=5;i>=-5;i--)
+        {for(int j=5;j>=s;j--)
+            {System.out.print(a);}
+        if(i>0)
+        {s++;
+        System.out.print(Math.abs(i));
+        for(int d=1;d<=2;d++)
+        for(int f=1;f<=h;f++)
+        {System.out.print(a);}
+        {System.out.println(Math.abs(i));
+        h++;}
+    }
+    else if(i<=0)
+    {s--;
+        System.out.print(Math.abs(i));
+        for(int d=1;d<=2;d++)
+        for(int f=1;f<=h;f++)
+        {System.out.print(a);}
+        {System.out.println(Math.abs(i));
+        h--;}
+}
+    }}}}
